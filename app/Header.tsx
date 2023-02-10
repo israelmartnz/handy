4c1284c3ff921be8ddc98b19601e3d93b069ab9c
CodeSandbox <support@codesandbox.io>
2023-02-10 05:15:44 +0000

    Initial commit

---
+++ app/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us!</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
